9aa20451b6dd57fb8d7deb0870a3a901be7de536
Goran Jumic <jumic.goran@gmail.com>
2021-01-11 13:15:00 +0100

    Variables as parameters, turn off, on and reset

--- mpower.sh
+++ mpower.sh
@@ -1,11 +1,52 @@
 #!/bin/bash
 
-MPOWER_USERNAME="admin"
-MPOWER_PASSWORD="2310gj2241"
-MPOWER_IP="10.114.3.103"
+SESSIONID="01234567890123456789012345678901"
 
-curl -s -X POST -d "username="$MPOWER_USERNAME"&password="$MPOWER_PASSWORD"" -b "AIROS_SESSIONID=01234567890123456789012345678901" "http://"$MPOWER_IP"/login.cgi"
+    while [[ $# -gt 0 ]]; do
+        opt="$1"
+        shift;
+        case "$opt" in
+            "-u"|"--user"               ) MPOWER_USERNAME="$1"; shift;;
+            "-p"|"--pass"               ) MPOWER_PASSWORD="$1"; shift;;
+            "-i"|"--ip"                 ) MPOWER_IP="$1"; shift;;
+			"-emeter"                   ) EMETER=TRUE;;
+			"-on"                       ) TURN_ON="$1"; shift;;
+			"-off"                      ) TURN_OFF="$1"; shift;;
+			"-restart"                  ) RESTART="$1"; shift;;
+            "-h"|"--help"               ) HELP=TRUE;;
+            "\?"                        ) echo "Unknown option: -$OPTARG" >&2; exit;;
+            ":"                         ) echo "Missing option argument for -$OPTARG" >&2; exit;;
+            *                           ) echo -e "Unimplemented argument\n Try:\n\t mpower.sh --help" >&2; exit;;
+        esac
+    done
 
-data=$(curl -b "AIROS_SESSIONID=01234567890123456789012345678901" -s "http://$MPOWER_IP/sensors")
+if [ X$HELP == "XTRUE" ]; then
+    echo -e " Syntax:\n\tmpower.sh -u <username> -p <password> -i <ip address> -emeter|-on <port number>|-off <port number>|-restart <port number>"
+	exit 0
+fi
 
-echo $data
+if [ -z "$MPOWER_USERNAME" ]; then
+    echo "Username not provided, exiting..."
+	exit 1
+fi
+if [ -z "$MPOWER_PASSWORD" ]; then
+    echo "Password not provided, exiting..."
+	exit 1
+fi
+if [ -z "$MPOWER_IP" ]; then
+    echo "Ip Address not provided, exiting..."
+	exit 1
+fi
+
+curl -s -X POST -d "username="$MPOWER_USERNAME"&password="$MPOWER_PASSWORD"" -b "AIROS_SESSIONID="$SESSIONID"" "http://"$MPOWER_IP"/login.cgi"
+
+if [ X$EMETER == "XTRUE" ]; then
+    
+    data=$(curl -b "AIROS_SESSIONID=$SESSIONID" -s "http://$MPOWER_IP/sensors")
+	echo $data
+fi
+
+[[ -z $TURN_ON ]] || curl --silent -X PUT -d output=1 -b "AIROS_SESSIONID="$SESSIONID $MPOWER_IP/sensors/$TURN_ON
+[[ -z $TURN_OFF ]] || curl --silent -X PUT -d output=0 -b "AIROS_SESSIONID="$SESSIONID $MPOWER_IP/sensors/$TURN_OFF 
+[[ -z $RESTART ]] || { curl --silent -X PUT -d output=0 -b "AIROS_SESSIONID="$SESSIONID $MPOWER_IP/sensors/$RESTART; sleep 5; curl --silent -X PUT -d output=1 -b "AIROS_SESSIONID="$SESSIONID $MPOWER_IP/sensors/$RESTART; }
+
